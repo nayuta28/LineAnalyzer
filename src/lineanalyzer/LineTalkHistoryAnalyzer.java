ebbf6687ba31c552325440495fe112a695145c3c
naoki-takashio <nayuta_1125@yahoo.co.jp>
2016-03-15 05:31:33 +0900

    add comment, rename class and method

--- src/lineanalyzer/LineTalkHistoryAnalyzer.java
+++ src/lineanalyzer/LineTalkHistoryAnalyzer.java
@@ -16,7 +16,28 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class LineAnalyzerMain {
+/**
+ * LINEのトーク履歴ファイルを解析する.
+ * [ファイルのフォーマット]
+ * ------------------------------
+ * [LINE] [グループ名]のトーク履歴
+ * 保存日時：yyyy/mm/dd HH:mm
+ * 
+ * yyyy/mm/dd(E)
+ * HH:mm \t メンバー \t 発言
+ * HH:mm \t メンバー \t 発言
+ * HH:mm \t メンバー \t 発言
+ * 
+ * yyyy/mm/dd(E)
+ * HH:mm \t メンバー \t 発言
+ * HH:mm \t メンバー \t 発言
+ * 			・
+ * 			・
+ * 			・
+ * ------------------------------
+ * @author nayuta28
+ */
+public class LineTalkHistoryAnalyzer {
 
 	private static Map<String, Long> memberCountMap = new LinkedHashMap<>();
 	private static Long totalTalkCount;
@@ -27,14 +48,18 @@ public class LineAnalyzerMain {
 
 			System.out.println("ファイルパスを入力してください");
 			String filePath = scan.nextLine();
+			// ファイルから全行を取得
 			List<String> allLinesList = readFile(filePath);
 
+			// メンバーごとの発言数をカウント
 			countMemberTalk(allLinesList);
-			showMemberTalkCount();
+			// 結果表示
+			outputMemberTalkCount();
 
 			System.out.println("検索したい文字列を入力してください");
-			String message = scan.nextLine();
-			countMessage(message, allLinesList);
+			String word = scan.nextLine();
+			// 履歴内の単語をカウント
+			countWord(word, allLinesList);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,19 +72,25 @@ public class LineAnalyzerMain {
 	}
 
 	private static void countMemberTalk(List<String> allLines) {
+		// 「時間 \t メンバー \t 発言」となっている行を抽出し、メンバーごとに発言数を集計する
 		memberCountMap = allLines.stream().map(s -> s.split("\t")).filter(s -> s.length >= 3)
 				.collect(Collectors.groupingBy(s -> s[1], Collectors.counting()));
+		// 発言数の昇順でソート
 		memberCountMap = sortMemberMap();
+		// 合計発言数を集計する
 		totalTalkCount = memberCountMap.values().stream().mapToLong(l -> l).sum();
 	}
 
-	private static void showMemberTalkCount() {
+	private static void outputMemberTalkCount() {
 		System.out.println("トーク履歴内のアカウントごとの発言数");
+		// 「メンバー名：発言数：発言率」で出力
 		memberCountMap.forEach((key, value) -> System.out.println(key + " : " + value + " : " + formatRate(value)));
 		System.out.println("合計発言数 ： " + totalTalkCount);
 	}
 
-	private static void countMessage(String word, List<String> allList) {
+	private static void countWord(String word, List<String> allList) {
+		// 行内で指定された単語が使われているか集計
+		// TODO:同じ行で同単語が使われている場合の集計方法
 		long wordCount = allList.stream().filter(s -> s.indexOf(word) > 0).count();
 		System.out.println("「" + word + "」は、" + wordCount + "回使われています");
 	}
